3a45a8bcbdd7aa0214da79af5e56d273e2a9f62f
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-20 16:16:25 -0400

    fixed mainInfo bg

--- app/(site)/(components)/MainInfo.tsx
+++ app/(site)/(components)/MainInfo.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react"
 
+import { Taviraj } from 'next/font/google';
+
+const taviraj_title = Taviraj({ weight: "300", subsets: ["latin"]})
+
 export default function MainInfo(){
   const [toggled, setToggled] = useState(false);
   
   return (
-    <div className="bg-Magnolia brightness-75 relative h-screen">
-      <div className="relative mt-[35vh] mb-[65vh] flex justify-center items-center">
-        {/* <div data-scroll data-scroll-speed="0.4" className="absolute w-[1400px] h-[475px] opacity-90 brightness-75 bg-Eerie-black"></div> */}
-        <div className="z-[3] relative text-Eerie-black text-center uppercase">
-          <h1 data-scroll data-scroll-speed="0.4" className="m-2 text-8xl">Data Consulting Services</h1>
+    <div className="bg-Magnolia h-screen top-[35vh] flex flex-col justify-center">
+      <div className="flex flex-col justify-center items-center">
+        <div data-scroll data-scroll-speed="0.1" className="absolute w-[1400px] h-3/4 brightness-70 bg-Eerie-black"></div>
+        <div data-scroll data-scroll-speed="0.2"  className=" relative text-Magnolia mb-[10vh] text-center uppercase" style={taviraj_title.style}>
+          <h1 className="text-8xl mt-1">Data Consulting Services</h1>
+          <h1 className="text-8xl mt-1">Data Consulting Services</h1>
+          <h1 className="text-8xl mt-1">Data Consulting Services</h1>
+          <h1 className="text-8xl mt-1">Data Consulting Services</h1>
         </div>
       </div>
     </div>
