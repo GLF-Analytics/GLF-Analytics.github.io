c8048272ae2c66c06a9883d6e7b7ae1d4da5a4d4
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-21 14:08:03 -0400

    fixed page sizing

--- app/(site)/(components)/index.tsx
+++ app/(site)/(components)/index.tsx
@@ -7,7 +7,7 @@ const taviraj_title = Taviraj({ weight: "300", subsets: ["latin"]})
 
 export default function Index(){
   return (
-    <div style={taviraj_title.style} className="h-screen flex justify-center">
+    <div style={taviraj_title.style} className="h-screen flex justify-center min-h-screen">
       <div className="absolute h-full w-full">
         <Image className="object-cover brightness-75" src={"/images/arch-1.jpg"} fill={true} alt='background image'/>
       </div>
@@ -15,13 +15,13 @@ export default function Index(){
         <div data-scroll data-scroll-speed="0.2" className="absolute w-[1400px] rounded-md h-[475px] opacity-90 brightness-70 bg-Eerie-black"></div>
         <div className="z-[3] relative text-Magnolia text-center uppercase flex flex-col items-center justify-center">
           <h1 data-scroll data-scroll-speed="0.3" className="text-[5rem] border-2 rounded-md brightness-70 bg-Eerie-black p-12">Data Consulting Services</h1>
-          <div data-scroll data-scroll-speed="0.4" className="text-4xl mt-16 border-2 rounded-md flex brightness-70 bg-Eerie-black flex-col justify-center p-4 lowercase">
+          <div data-scroll data-scroll-speed="0.4" className="text-4xl mt-16 border-2 rounded-md brightness-70 bg-Eerie-black flex-col justify-center p-4 lowercase">
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                'Reporting',
-                2000, // wait 1s before replacing "Mice" with "Hamsters"
                 'Growth Analytics',
+                2000,
+                'Reporting',
                 2000,
                 'Dashboard Creation',
                 2000,
@@ -31,6 +31,8 @@ export default function Index(){
               wrapper="span"
               speed={50}
               repeat={Infinity}
+              preRenderFirstString={true}
+              cursor={true}
             />
           </div>
         </div>
