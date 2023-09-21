84576fbcea3f5d4d37dea87449ecc90041726e6b
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-09-21 12:28:00 -0400

    added animated text & updated styling for topview

--- app/(site)/(components)/index.tsx
+++ app/(site)/(components)/index.tsx
@@ -1,24 +1,37 @@
 import Image from "next/image"
 
 import { Taviraj } from 'next/font/google';
+import { TypeAnimation } from "react-type-animation";
 
 const taviraj_title = Taviraj({ weight: "300", subsets: ["latin"]})
 
 export default function Index(){
   return (
-    <div style={taviraj_title.style}>
-      <div className="absolute h-[130vh] w-full">
+    <div style={taviraj_title.style} className="h-screen flex justify-center">
+      <div className="absolute h-full w-full">
         <Image className="object-cover brightness-75" src={"/images/arch-1.jpg"} fill={true} alt='background image'/>
       </div>
-      <div className="relative mt-[35vh] mb-[60vh] flex justify-center items-center">
-        <div data-scroll data-scroll-speed="0.4" className="absolute w-[1400px] h-[475px] opacity-90 brightness-70 bg-Eerie-black"></div>
-        <div className="z-[3] relative text-Magnolia text-center uppercase">
-          <h1 data-scroll data-scroll-speed="0.4" className="m-2 text-8xl">Data Consulting Services</h1>
-          <div data-scroll data-scroll-speed="0.4" className="text-4xl mt-16">
-            <h1 className="m-2">R e p o r t i n g</h1>
-            <h1 className="m-2">&</h1>
-            <h1 className="m-2">G r o w t h</h1>
-            <h1 className="m-2">A n a l y t i c s</h1>
+      <div className="relative flex flex-col justify-center items-center -mt-24">
+        <div data-scroll data-scroll-speed="0.2" className="absolute w-[1400px] rounded-md h-[475px] opacity-90 brightness-70 bg-Eerie-black"></div>
+        <div className="z-[3] relative text-Magnolia text-center uppercase flex flex-col items-center justify-center">
+          <h1 data-scroll data-scroll-speed="0.3" className="text-[5rem] border-2 rounded-md brightness-70 bg-Eerie-black p-12">Data Consulting Services</h1>
+          <div data-scroll data-scroll-speed="0.4" className="text-4xl mt-16 border-2 rounded-md flex brightness-70 bg-Eerie-black flex-col justify-center p-4 lowercase">
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Reporting',
+                2000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Growth Analytics',
+                2000,
+                'Dashboard Creation',
+                2000,
+                'Data Warehousing',
+                2000
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </div>
         </div>
       </div>
