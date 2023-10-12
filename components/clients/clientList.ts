49152c873dc40cc64d9cb82d338f2fc70f3331e7
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-10-12 01:51:02 -0400

    updated image paths

--- components/clients/clientList.ts
+++ components/clients/clientList.ts
@@ -1,40 +1,40 @@
 export type ClientProps = {
-  clientName: string,
-  link: string,
-  src: string
-}
+  clientName: string;
+  link: string;
+  src: string;
+};
 
 export type ClientList = ClientProps[];
 
-export const clientList:ClientList  = [
+export const clientList: ClientList = [
   {
-    clientName: "Dove",
-    link: "https://www.dove.com/us/en/home.html",
-    src: "/images/clients/Dove.png"
+    clientName: 'Dove',
+    link: 'https://www.dove.com/us/en/home.html',
+    src: '/images/clients/Dove-300x200.png',
   },
   {
-    clientName: "UCAN",
-    link: "https://ucan.co/",
-    src: "/images/clients/UCAN.png"
+    clientName: 'UCAN',
+    link: 'https://ucan.co/',
+    src: '/images/clients/UCAN-300x200.png',
   },
   {
-    clientName: "Kreatures of Habit",
-    link: "https://kreaturesofhabit.com/",
-    src:"/images/clients/KoH.png"
+    clientName: 'Kreatures of Habit',
+    link: 'https://kreaturesofhabit.com/',
+    src: '/images/clients/KOH-300x200.png',
   },
   {
-    clientName: "Indie Consulting",
-    link: "https://indie-consulting.com/",
-    src:"/images/clients/Indie+Consulting.png"
+    clientName: 'Indie Consulting',
+    link: 'https://indie-consulting.com/',
+    src: '/images/clients/Indie-Consulting-300x200.png',
   },
   {
-    clientName: "Your Super",
-    link: "https://www.dove.com/us/en/home.html",
-    src:"/images/clients/YS.png"
+    clientName: 'Your Super',
+    link: 'https://www.dove.com/us/en/home.html',
+    src: '/images/clients/Your-Super-300x200.png',
   },
   {
-    clientName: "Unilever",
-    link: "https://www.unilever.com/",
-    src:"/images/clients/UL.png"
+    clientName: 'Unilever',
+    link: 'https://www.unilever.com/',
+    src: '/images/clients/Unilever-300x200.png',
   },
-]
+];
