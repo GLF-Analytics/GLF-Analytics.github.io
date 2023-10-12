c86def2f521151fb226486ef17e43edb577c65f5
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-10-12 11:11:58 -0400

    added hover content to table

--- components/customdatasolutions/CustomTable.tsx
+++ components/customdatasolutions/CustomTable.tsx
@@ -8,10 +8,12 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 import { Check } from 'lucide-react';
 
 const CustomTable = () => {
@@ -20,7 +22,7 @@ const CustomTable = () => {
       <h2 className="text-center text-5xl">Custom Data Solutions</h2>
       <div className="pt-6 text-center">
         <Table className="overflow-hidden text-2xl">
-          <TableCaption>Click option to see table items</TableCaption>
+          <TableCaption>Hover over option to see table items</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="text-center">Option Name</TableHead>
@@ -38,10 +40,22 @@ const CustomTable = () => {
             </TableRow>
             <TableRow>
               <TableCell>
-                <Popover>
-                  <PopoverTrigger>Orders Table</PopoverTrigger>
-                  <PopoverContent></PopoverContent>
-                </Popover>
+                <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Orders Table</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>order_id</p>
+                      <p>date_ordered</p>
+                      <p>order_total</p>
+                      <p>discount_total</p>
+                      <p>shipping_total</p>
+                      <p>customer_id</p>
+                      <p>order_status</p>
+                      <p>customer_type</p>
+                      <p>date_week</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               </TableCell>
               <TableCell>
                 <div className="flex justify-center">
@@ -61,10 +75,27 @@ const CustomTable = () => {
             </TableRow>
             <TableRow>
               <TableCell>
-                <Popover>
-                  <PopoverTrigger>Retention Table</PopoverTrigger>
-                  <PopoverContent></PopoverContent>
-                </Popover>
+                <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Retention Table</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>order_id</p>
+                      <p>date_ordered</p>
+                      <p>order_total</p>
+                      <p>discount_total</p>
+                      <p>shipping_total</p>
+                      <p>customer_id</p>
+                      <p>order_status</p>
+                      <p>customer_type</p>
+                      <p>date_first_purchase</p>
+                      <p>retained_30_days</p>
+                      <p>retained_90_days</p>
+                      <p>retained_180_days</p>
+                      <p>retained_360_days</p>
+                      <p>date_week</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               </TableCell>
               <TableCell>
                 <div className="flex justify-center">
@@ -83,7 +114,25 @@ const CustomTable = () => {
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Basic Reporting Overviews</TableCell>
+              <TableCell>
+              <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Basic Reporting Overviews</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>month</p>
+                      <p>unique orders</p>
+                      <p>sales</p>
+                      <p>aov</p>
+                      <p>unique orders - new</p>
+                      <p>sales - new</p>
+                      <p>aov - new</p>
+                      <p>unique orders - returning</p>
+                      <p>sales - returning</p>
+                      <p>aovv - returning</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
               <TableCell>
                 <div className="flex justify-center">
                   <Check />
@@ -101,7 +150,28 @@ const CustomTable = () => {
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Line Items Table</TableCell>
+              <TableCell>
+              <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Line Items Table</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>order_id</p>
+                      <p>date_ordered</p>
+                      <p>order_total</p>
+                      <p>discount_total</p>
+                      <p>shipping_total</p>
+                      <p>customer_id</p>
+                      <p>order_status</p>
+                      <p>customer_type</p>
+                      <p>item_total</p>
+                      <p>item_quantity</p>
+                      <p>item_name</p>
+                      <p>item_price</p>
+                      <p>date_week</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
               <TableCell></TableCell>
               <TableCell>
                 <div className="flex justify-center">
@@ -115,7 +185,21 @@ const CustomTable = () => {
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Google Analytics Table</TableCell>
+              <TableCell>
+              <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Google Analytics Table</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>total_revenue</p>
+                      <p>conversions</p>
+                      <p>date_ordered</p>
+                      <p>total_users</p>
+                      <p>sessions</p>
+                      <p>date_week</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
               <TableCell></TableCell>
               <TableCell>
                 <div className="flex justify-center">
@@ -129,7 +213,33 @@ const CustomTable = () => {
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Discount Codes Logic</TableCell>
+              <TableCell>
+              <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Discount Codes Logic</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>order_id</p>
+                      <p>date_ordered</p>
+                      <p>order_total</p>
+                      <p>discount_total</p>
+                      <p>shipping_total</p>
+                      <p>customer_id</p>
+                      <p>order_status</p>
+                      <p>customer_type</p>
+                      <p>date_first_purchase</p>
+                      <p>retained_30_days</p>
+                      <p>retained_90_days</p>
+                      <p>retained_180_days</p>
+                      <p>retained_360_days</p>
+                      <p>date_week</p>
+                      <p>code_purchaser</p>
+                      <p>code_name</p>
+                      <p>acquired_via_code</p>
+                      <p>orders_count_all_time</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
               <TableCell></TableCell>
               <TableCell></TableCell>
               <TableCell>
@@ -139,7 +249,21 @@ const CustomTable = () => {
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>Advanced Reporting Overview</TableCell>
+              <TableCell>
+              <TooltipProvider>
+                  <Tooltip delayDuration={0}>
+                    <TooltipTrigger>Advanced Reporting Overviews</TooltipTrigger>
+                    <TooltipContent side="bottom" className="text-2xl">
+                      <p>date-year-month</p>
+                      <p>total spend</p>
+                      <p>new customers</p>
+                      <p>total revenue</p>
+                      <p>cac</p>
+                      <p>roi</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </TableCell>
               <TableCell></TableCell>
               <TableCell></TableCell>
               <TableCell>
