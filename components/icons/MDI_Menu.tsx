c2e941f1d5bf9b69e99e9d93c4dc8ec96adcca9c
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-10-16 22:50:10 -0400

    added nav menu

---
+++ components/icons/MDI_Menu.tsx
@@ -0,0 +1,12 @@
+export default function MDI_Menu({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="36"
+      viewBox="0 0 24 24"
+    >
+      <path fill="#efe9f4" d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z" />
+    </svg>
+  );
+}
---
+++ components/navbar/SheetTop.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetClose,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { RefObject } from "react";
+import MDI_Menu from "../icons/MDI_Menu";
+import { Button } from "../ui/button";
+import { Taviraj } from "next/font/google";
+
+const taviraj_logo = Taviraj({
+  weight: '300',
+  subsets: ['latin'],
+  style: 'italic',
+});
+
+type NavbarProps = {
+  top: RefObject<HTMLDivElement>;
+  faq: RefObject<HTMLDivElement>;
+  clients: RefObject<HTMLDivElement>;
+  customDataSolutions: RefObject<HTMLDivElement>;
+  media: RefObject<HTMLDivElement>;
+  footer: RefObject<HTMLDivElement>;
+};
+
+const SheetTop = (props: NavbarProps) => {
+  const scrollToComponent = (ref: React.RefObject<HTMLDivElement>, position:ScrollLogicalPosition ) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth', block: position });
+    }
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger><MDI_Menu/></SheetTrigger>
+      <SheetContent className="w-screen flex flex-col justify-start items-center" side={"top"}>
+        <SheetHeader style={taviraj_logo.style} className="text-4xl font-bold">Navigation</SheetHeader>
+        <SheetFooter>
+          <SheetClose className="mx-auto">
+            <div className="flex flex-col justify-center items-center">
+              <Button
+                  style={taviraj_logo.style}
+                  onClick={() => scrollToComponent(props.top, "start")}
+                  variant="ghost"
+                  className="text-2xl"
+                >
+                  Home
+              </Button>
+              <Button
+                style={taviraj_logo.style}
+                onClick={() => scrollToComponent(props.faq, "start")}
+                variant="ghost"
+                className="text-2xl"
+              >
+                FAQ
+              </Button>
+              <Button
+                style={taviraj_logo.style}
+                onClick={() => scrollToComponent(props.clients, "center")}
+                variant="ghost"
+                className="text-2xl"
+              >
+                Clients
+              </Button>
+              <Button
+                style={taviraj_logo.style}
+                onClick={() => scrollToComponent(props.customDataSolutions, "center")}
+                variant="ghost"
+                className="text-2xl whitespace-nowrap"
+              >
+                Custom Data Solutions
+              </Button>
+              <Button
+                style={taviraj_logo.style}
+                onClick={() => scrollToComponent(props.media, "start")}
+                variant="ghost"
+                className="text-2xl"
+              >
+                Media
+              </Button>
+              <Button
+                style={taviraj_logo.style}
+                onClick={() => scrollToComponent(props.footer, "start")}
+                variant="ghost"
+                className="text-2xl"
+              >
+                Contact
+              </Button>
+            </div>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default SheetTop;
