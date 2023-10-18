c35afd0b59d1a0efb05132c7af454aa19921a2a7
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-10-18 17:31:50 -0400

    added copy to clipboard & linkedin buttons

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,23 +1,48 @@
-import { Taviraj } from 'next/font/google';
-import { forwardRef } from 'react';
-import { Button } from '../ui/button';
+import { Taviraj } from "next/font/google";
+import { forwardRef, useState } from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { useCopyToClipboard } from "usehooks-ts";
 
-const taviraj_title = Taviraj({ weight: '300', subsets: ['latin'] });
+const taviraj_title = Taviraj({ weight: "300", subsets: ["latin"] });
 
 const Footer = () => {
+  const [value, copy] = useCopyToClipboard();
+  const [isCopied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    copy("gabrielf@glfanalytics.com");
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
   return (
     <footer
       style={taviraj_title.style}
-      className="flex flex-col justify-center gap-4 xl:gap-8 items-center h-[25vh] bg-Eerie-black text-Magnolia"
+      className="flex h-[25vh] flex-col items-center justify-center gap-4 bg-Eerie-black text-Magnolia xl:h-[35vh] xl:gap-16"
     >
       <h2 className="text-2xl xl:text-5xl">Get in touch.</h2>
       <Button
         style={taviraj_title.style}
         variant="ghost"
-        className="text-lg xl:text-4xl bg-Magnolia p-4 xl:p-8 text-Eerie-black w-fit"
+        className="w-fit bg-Magnolia p-4 text-lg text-Eerie-black xl:p-8 xl:text-4xl"
+        onClick={handleCopy}
       >
-        gabrielf@glfanalytics.com
+        {!isCopied ? "gabrielf@glfanalytics.com" : "Copied."}
       </Button>
+      <Link href="https://www.linkedin.com/in/gabriel-freeman-47b80389/">
+        <Image
+          src="/icons/linkedin.svg"
+          alt="linkedin logo"
+          width={50}
+          height={50}
+          className="w-6 sm:w-8 xl:w-10"
+        ></Image>
+      </Link>
     </footer>
   );
 };
